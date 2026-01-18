18ae6a5228326da4669907980a4c6a04d4891562
Renan da S. Silvaira <silveira.s.renan@gmail.com>
2026-01-18 19:52:11 -0300

    commit inicial

---
+++ src/main/java/com/fiap/vaga_liberada_notificacao/VagaLiberadaNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.vaga_liberada_notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaLiberadaNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagaLiberadaNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/vaga_liberada_notificacao/VagaLiberadaNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.vaga_liberada_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaLiberadaNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
